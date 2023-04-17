f37a3b3fc7b02080c1f92226bfa63aeb7e692d29
happyeveryone96 <happyeveryone@naver.com>
2023-04-17 15:07:43 +0900

    refactor: routerPush 함수 관련 코드 정리

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -4,17 +4,17 @@ import { useRouter } from 'next/router';
 
 const Sort: React.FC = () => {
   const router = useRouter();
-  const routerPush = (e: any) => {
-    if (e.target.value === 'default') {
-      router.push('/');
-    } else {
-      router.push(`?price=${e.target.value}`);
-    }
+  const routerPush = (e: 'default' | 'lowToHigh' | 'highToLow') => {
+    if (e === 'default') router.push('/');
+    else router.push(`?price=${e}`);
   };
 
   return (
     <div className={css.container}>
-      <select className={css.sortBox} onChange={routerPush}>
+      <select
+        className={css.sortBox}
+        onChange={(e) => routerPush(e.target.value)}
+      >
         <option value="default">Price ($)</option>
         <option value="lowToHigh">Low to High</option>
         <option value="highToLow">High to Low</option>
